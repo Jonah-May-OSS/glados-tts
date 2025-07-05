7fdb47e6e536384e1b7bf13deb138f2d9a316399
JonahMMay <jonah@mayfamily.me>
2025-07-05 14:35:03 +0000

    Upload model files

---
+++ models/tacotron-trt.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7c556ed5a1279826b0053d8655056941b5b5ca8b72e712b6dc3a84e8a6f29d41
+size 116161427
---
+++ models/vocoder-trt.ts
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8771d555986c0b5f97f22543ee7b2a25a6628dc4db2b03307b3d273f063bd555
+size 42751323
